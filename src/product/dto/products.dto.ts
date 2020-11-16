fec0a49547c57a3164b3bad572e379e87a6c7ebb
Facu Conci <facu@example.com>
2020-11-16 12:40:59 -0300

    basics

---
+++ src/product/dto/products.dto.ts
@@ -0,0 +1,7 @@
+export class CreateProductDTO {
+    name: string;
+    unitPrice: number;
+    stock: number;
+    rubro: string;
+    createdAt: Date;
+}
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import { Res, Req, HttpStatus, Body, } from '@nestjs/common';
+import { CreateProductDTO } from './dto/products.dto';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+
+    @Post('create')
+    postProduct(@Res() res, @Body() createProductDTO: CreateProductDTO ) {
+        console.log(createProductDTO);
+        return res.json({
+            message: 'created',
+        });
+    }
+
+    @Get()
+    getProducts(@Res() res, @Req() req) {
+        return res.json({
+            message: 'received',
+        });
+    }
+
+}
