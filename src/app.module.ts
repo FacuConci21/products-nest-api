7891d17af7b78abbf912caf3c144b2cbdab79190
Facu Conci <facu@example.com>
2020-11-25 04:50:11 -0300

    environment configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [
     ProductModule, 
+    ConfigModule.forRoot({ isGlobal: true, }),
     MongooseModule.forRoot('mongodb://localhost/products-db', 
       {useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology:true}
       ),
